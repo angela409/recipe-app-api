6312a3412a4a503a40f690fcbed9e1ebb24eebb6
Angela Stojanovska <angela.stojanovska@3fs.si>
2022-10-06 16:01:19 +0200

    S-16 Add image to recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,9 @@
 """
 Views for the recipe APIs
 """
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -26,8 +28,22 @@ class RecipeViewSet(viewsets.ModelViewSet):
            Specify which serializer class to be returned and when."""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerialzer
 
         return self.serializers_class
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
     def perform_create(self, serializer):
         """Create a new recipe."""
